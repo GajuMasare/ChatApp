081851e5f0628e1bf4d78a47442ac3ba3345455d
Gaju Masare <113698192+GajuMasare@users.noreply.github.com>
2024-09-07 14:47:44 +0530

    mongo added and connected

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,21 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connetToMongoDB.js";
 
 const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello world~~~~");
 });
 
-app.listen(5000, () => console.log(`server running on port 5000`));
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`server running on port ${PORT}`);
+});
