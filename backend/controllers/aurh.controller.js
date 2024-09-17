cbc304dd30e649630ba20013c520b447f617a56a
Gaju Masare <113698192+GajuMasare@users.noreply.github.com>
2024-09-17 13:38:04 +0530

    logout done

--- backend/controllers/aurh.controller.js
+++ backend/controllers/aurh.controller.js
@@ -77,6 +77,12 @@ export const login = async (req, res) => {
   }
 };
 
-export const logout = (req, res) => {
-  console.log("logoutUser");
+export const logout = async (req, res) => {
+  try {
+    res.cookie("jwt", "", { maxAge: 0 }); //crealring all jwt tokens
+    res.status(200).json({ message: "Logged out sussfully MF" });
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
