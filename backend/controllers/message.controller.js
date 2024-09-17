1798961ede789a3a53a8ebcb644ccac2940606ad
Gaju Masare <113698192+GajuMasare@users.noreply.github.com>
2024-09-17 16:39:19 +0530

    get messages endpoint is added

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -30,10 +30,30 @@ export const sendMessage = async (req, res) => {
     // await conversation.save();
     // await newMessage.save();
 
-    //this will work pallralel
+    //this will work pallralel~
     await Promise.all([conversation.save(), newMessage.save()]);
 
     res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("error in sendMessage conroller: ", error.message);
+    res.status(500).json({ error: "internal server error" });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userToChatId] },
+    }).populate("messages"); //not ref but actual message
+
+    if (!conversation) return res.status(200).json([]);
+
+    const messages = conversation.messages;
+
+    res.status(200).json(messages);
   } catch (error) {
     console.log("error in sendMessage conroller: ", error.message);
     res.status(500).json({ error: "internal server error" });
